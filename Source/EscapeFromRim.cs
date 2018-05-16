9db1c6a6b488a493c9beb47591499d057b75235e
Dutchedark <33281828+Dutchedark@users.noreply.github.com>
2018-05-16 15:32:17 +0200

    Sample makarov
    Sample xml

--- Source/EscapeFromRim.cs
+++ Source/EscapeFromRim.cs
@@ -1,6 +1,13 @@
-﻿namespace EscapeFromRim
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace EscapeFromRim
 {
     public class EscapeFromRim
     {
+        //Log.Message("It is me, Soggy!");
     }
 }
